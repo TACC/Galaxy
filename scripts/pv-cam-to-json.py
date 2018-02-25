55901df24697c98cf1bd3b3e29e30728b071529d
Gregory Abram <gda@login1.hikari.tacc.utexas.edu>
2018-02-25 17:01:25 -0600

    scripts

---
+++ scripts/pv-cam-to-json.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import xmltodict
+
+if len(sys.argv) != 3:
+		print "syntax: camera.xml  camera.json"
+		sys.exit(0)
+
+with open(sys.argv[1], 'r') as f:
+    xmlString = f.read()
+ 
+jsonString = json.dumps(xmltodict.parse(xmlString), indent=4)
+ 
+with open(sys.argv[2], 'w') as f:
+    f.write(jsonString)
