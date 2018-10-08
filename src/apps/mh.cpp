535274c32fbb5972413aee65d452a11dbe31626d
Gregory Abram <gda@login1.hikari.tacc.utexas.edu>
2018-10-08 08:57:48 -0500

    gaussian, etc

--- src/apps/mh.cpp
+++ src/apps/mh.cpp
@@ -27,14 +27,16 @@
 #include <math.h>
 
 #include <dtypes.h>
-#include "Application.h"
+#include <Application.h>
 #include "Renderer.h"
 #include "RenderingSet.h"
-#include "MHSampleMsg.h"
 
 #include <ospray/ospray.h>
 
 #include <time.h>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
 
 using namespace boost;
 
@@ -54,15 +56,250 @@ int width  = WIDTH;
 int height = HEIGHT;
 
 float radius = 0.02;
+float target = 0.0;
+float power = 10.0;
 float sigma = 1.0;
 int   max_consecutive_misses = 10;
-
-int   iteration_limit = 200000;
+int   iteration_limit = 20000;
 int   hot_start = 1000;
 float scaling = 1.0;
 int   skip = 10;
-float data_min = -1, data_max = -1;
-bool  data_minmax_from_argument = false;
+
+#define TF_NONE     0
+#define TF_LINEAR   1
+#define TF_GAUSSIAN 2
+
+
+int tf_type = TF_NONE;
+float tf0, tf1;
+
+variate_generator<mt19937, normal_distribution<> > *generator;
+
+void normally_distributed_vector(double* v)
+{
+  for (size_t i = 0; i < 3; ++i)
+    v[i]=(*generator)();
+}
+
+float gaussian(float x, float m, float s)
+{
+  return ( 1 / ( s * sqrt(2*M_PI) ) ) * exp( -0.5 * pow( (x-m)/s, 2.0 ) );
+}
+
+#define RNDM ((float)rand() / RAND_MAX) 
+
+class SampleMsg : public Work
+{
+    struct args
+    {
+        Key   vk;
+        Key   pk;
+        int   tf_type;
+        float tf0;
+        float tf1;
+        float step;
+        int   n_iterations;
+        int   n_start;
+        int   n_skip;
+        int   n_miss;
+    };
+
+public:
+  SampleMsg(VolumeP v, ParticlesP p, int tf_type, float tf0, float tf1, float step, int niterations, int nstart, int nskip, int nmiss)
+            : SampleMsg(sizeof(args))
+  {
+        args *a = (args *)contents->get();
+        a->vk = v->getkey();
+        a->pk = p->getkey();
+        a->tf_type = tf_type;
+        a->tf0 = tf0;
+        a->tf1 = tf1;
+        a->step = step;
+        a->n_iterations = niterations;
+        a->n_start = nstart;
+        a->n_skip = nskip;
+        a->n_miss = nmiss;
+    }
+
+  ~SampleMsg() {}
+
+  WORK_CLASS(SampleMsg, true)
+
+private: 
+  float istep, jstep, kstep;
+
+    vec3f get_starting_point(VolumeP v)
+    {
+        Box *box = v->get_local_box();
+        return vec3f(box->xyz_min.x + RNDM*(box->xyz_max.x - box->xyz_min.x),
+                     box->xyz_min.y + RNDM*(box->xyz_max.y - box->xyz_min.y),
+                     box->xyz_min.z + RNDM*(box->xyz_max.z - box->xyz_min.z));
+    }
+
+  float Q(VolumeP v, float s, args *a)
+  {
+    float q;
+
+    if (a->tf_type == TF_LINEAR)
+    {
+      if (a->tf0 < a->tf1)
+      {
+        if (s < a->tf0) q = 0.0;
+        else if (s > a->tf1) q = 1.0;
+        else q = (s - a->tf0) / (a->tf1 - a->tf0);
+      }
+      else
+      {
+        if (s < a->tf1) q = 1.0;
+        else if (s > a->tf0) q = 0.0;
+        else q = (s - a->tf0) / (a->tf1 - a->tf0);
+      }
+    }
+    else
+    {
+      q = gaussian(s, a->tf0, a->tf1);
+    }
+
+    return q;
+  }
+
+  float sample(VolumeP v, Particle& p) { return sample(v, p.xyz.x, p.xyz.y, p.xyz.z); }
+  float sample(VolumeP v, vec3f xyz) { return sample(v, xyz.x, xyz.y, xyz.z); }
+  float sample(VolumeP v, float x, float y, float z)
+  {
+    float dx, dy, dz;
+    v->get_deltas(dx, dy, dz);
+
+    float ox, oy, oz;
+    v->get_ghosted_local_origin(ox, oy, oz);
+
+    x = (x - ox) / dx;
+    y = (y - oy) / dy;
+    z = (z - oz) / dz;
+
+    int ix = (int)x;
+    int iy = (int)y;
+    int iz = (int)z;
+
+    dx = x - ix;
+    dy = y - iy;
+    dz = z - iz;
+
+    int v000 = (ix + 0) * istep + (iy + 0) * jstep + (iz + 0) * kstep;
+    int v001 = (ix + 0) * istep + (iy + 0) * jstep + (iz + 1) * kstep;
+    int v010 = (ix + 0) * istep + (iy + 1) * jstep + (iz + 0) * kstep;
+    int v011 = (ix + 0) * istep + (iy + 1) * jstep + (iz + 1) * kstep;
+    int v100 = (ix + 1) * istep + (iy + 0) * jstep + (iz + 0) * kstep;
+    int v101 = (ix + 1) * istep + (iy + 0) * jstep + (iz + 1) * kstep;
+    int v110 = (ix + 1) * istep + (iy + 1) * jstep + (iz + 0) * kstep;
+    int v111 = (ix + 1) * istep + (iy + 1) * jstep + (iz + 1) * kstep;
+
+    float b000 = (1.0 - dx) * (1.0 - dy) * (1.0 - dz);
+    float b001 = (1.0 - dx) * (1.0 - dy) * (dz);
+    float b010 = (1.0 - dx) * (dy) * (1.0 - dz);
+    float b011 = (1.0 - dx) * (dy) * (dz);
+    float b100 = (dx) * (1.0 - dy) * (1.0 - dz);
+    float b101 = (dx) * (1.0 - dy) * (dz);
+    float b110 = (dx) * (dy) * (1.0 - dz);
+    float b111 = (dx) * (dy) * (dz);
+
+    if (v->get_type() == Volume::FLOAT)
+    {
+      float *p = (float *)v->get_samples();
+      return p[v000]*b000 + p[v001]*b001 + p[v010]*b010 + p[v011]*b011 +
+             p[v100]*b100 + p[v101]*b101 + p[v110]*b110 + p[v111]*b111;
+    }
+    else
+    {
+      unsigned char *p = (unsigned char *)v->get_samples();
+      return p[v000]*b000 + p[v001]*b001 + p[v010]*b010 + p[v011]*b011 +
+             p[v100]*b100 + p[v101]*b101 + p[v110]*b110 + p[v111]*b111;
+    }
+  }
+
+public:
+  bool CollectiveAction(MPI_Comm c, bool s)
+  {
+    args *a = (args *)contents->get();
+
+    VolumeP v = Volume::Cast(KeyedDataObject::GetByKey(a->vk));
+    ParticlesP p = Particles::Cast(KeyedDataObject::GetByKey(a->pk));
+
+    p->CopyPartitioning(v);
+
+    float deltaX, deltaY, deltaZ;
+    v->get_deltas(deltaX, deltaY, deltaZ);
+
+    float ox, oy, oz;
+    v->get_local_origin(ox, oy, oz);
+
+    int lioff, ljoff, lkoff;
+    v->get_ghosted_local_offsets(lioff, ljoff, lkoff);
+
+    int nli, nlj, nlk;
+    v->get_local_counts(nli, nlj, nlk);
+
+    int gnli, gnlj, gnlk;
+    v->get_ghosted_local_counts(gnli, gnlj, gnlk);
+
+    int local_count = nli*nlj*nlk;
+
+    float max_x = ox + (nli-1) * deltaX,
+          max_y = oy + (nlj-1) * deltaY,
+          max_z = oz + (nlk-1) * deltaZ;
+
+    int ngx, ngy, ngz;
+    v->get_local_counts(ngx, ngy, ngz);
+    int global_count = ngx*ngy*ngz;
+
+    istep = 1;
+    jstep = gnli;
+    kstep = gnli * gnlj;
+
+    Particle tp;
+    tp.xyz = get_starting_point(v);
+    tp.u.value = sample(v, tp.xyz);
+
+    float tq = Q(v, tp.u.value, a);
+
+    Box *partition_box = v->get_local_box();
+
+    int miss_count = 0;
+    for (int iteration = 0; iteration < (a->n_start + a->n_iterations); )
+    {
+      double rv[3];
+      normally_distributed_vector(rv);
+
+      Particle cp(tp.xyz.x + rv[0], tp.xyz.y + rv[1], tp.xyz.z + rv[2], float(0.0));
+      if (! partition_box->isIn(cp.xyz)) continue;
+
+      iteration ++;
+
+      cp.u.value = sample(v, cp);
+      float cq = Q(v, cp.u.value, a);
+
+      if ((cq > tq) || (RNDM < (cq/tq)))
+      {
+        tp = cp;
+        tq = cq;
+        if ((iteration > a->n_start) && ((iteration % a->n_skip) == 0))
+          p->push_back(tp);
+        miss_count = 0;
+      }
+      else if (++miss_count > a->n_miss)
+      {
+        tp.xyz = get_starting_point(v);
+        tp.u.value = sample(v, tp.xyz);
+        miss_count = 0;
+      }
+    }
+
+    std::cerr << "created " << p->get_n_samples() << " samples\n";
+    return false;
+  }
+};
+
+WORK_CLASS_TYPE(SampleMsg)
 
 void
 syntax(char *a)
@@ -71,23 +308,27 @@ syntax(char *a)
   cerr << "optons:" << endl;
   cerr << "  -D            run debugger" << endl;
   cerr << "  -M m          max number of consecutive misses allowed (" << max_consecutive_misses << ")" << "\n";
-  cerr << "  -i n          iteration limit (" << iteration_limit << ")\n";
-  cerr << "  -h n          hot start count (" << hot_start << ")\n";
-  cerr << "  -S step       scaling of candidate step (" << scaling << ")\n";
-  cerr << "  -K skip       record every skip'th successful sample (" << skip << ")\n";
-  cerr << "  -R p0 p1      values at which probability is 0 and 1, extended (data range min to max)\n";
+  cerr << "  -i n          iteration limit (20000)\n";
+  cerr << "  -h n          hot start count (1000)\n";
+  cerr << "  -S step       scaling of candidate step (1)\n";
+  cerr << "  -K skip       record every skip'th successful sample (10)\n";
+  cerr << "  -R p0 p1      linear transfer function, from p0 to p1 extended (default, linear data range)\n";
+  cerr << "  -G p0 p1      gaussian transfer function p0 is median, p2 is stdev (default, linear data range)\n";
   cerr << "  -s x y        window size (" << WIDTH << "x" << HEIGHT << ")" << endl;
+  cerr << "  -t target     value of highest interest (" << target << ")" << endl;
+  cerr << "  -p power      exponent of interest (" << power << ")" << endl;
   cerr << "  -r radius     radius of samples (" << radius << ")" << endl;
   cerr << "  -k sigma      standard deviation of normally distributed random step vector (" << sigma << ")" << endl;
   exit(1);
 }
+
 
 int
 main(int argc, char * argv[])
 {
   string data = "";
   char *dbgarg;
-      bool dbg = false;
+  bool dbg = false;
 
   ospInit(&argc, (const char **)argv);
 
@@ -106,8 +347,11 @@ main(int argc, char * argv[])
         case 'h': hot_start = atoi(argv[++i]); break;
         case 'S': scaling = atof(argv[++i]); break;
         case 'K': skip = atoi(argv[++i]); break;
-        case 'R': data_min = atof(argv[++i]), data_max = atof(argv[++i]), data_minmax_from_argument = true; break;
+        case 'R': tf0 = atof(argv[++i]), tf1 = atof(argv[++i]), tf_type = TF_LINEAR; break;
+        case 'G': tf0 = atof(argv[++i]), tf1 = atof(argv[++i]), tf_type = TF_GAUSSIAN; break;
         case 'r': radius = atof(argv[++i]); break;
+        case 't': target = atof(argv[++i]); break;
+        case 'p': power  = atof(argv[++i]); break;
         case 'k': sigma = atof(argv[++i]); break;
         case 's': width  = atoi(argv[++i]); height = atoi(argv[++i]); break;
         default:
@@ -132,20 +376,20 @@ main(int argc, char * argv[])
 
   Debug *d = dbg ? new Debug(argv[0], false, dbgarg) : NULL;
 
-  MHSampleMsg::Register();
+  SampleMsg::Register();
 
   if (mpiRank == 0)
   {
     VolumeP volume = Volume::NewP();
     volume->Import(data);
 
-    if (! data_minmax_from_argument)
-      volume->get_global_minmax(data_min, data_max);
+    if (tf_type == TF_NONE)
+      volume->get_global_minmax(tf0, tf1), tf_type = TF_LINEAR;
 
     ParticlesP samples = Particles::NewP();
     samples->SetRadius(radius);
 
-    MHSampleMsg *smsg = new MHSampleMsg(volume, samples, data_min, data_max, scaling, iteration_limit, hot_start, skip, max_consecutive_misses);
+    SampleMsg *smsg = new SampleMsg(volume, samples, tf_type, tf0, tf1, scaling, iteration_limit, hot_start, skip, max_consecutive_misses);
     smsg->Broadcast(true, true);
 
     samples->Commit();
