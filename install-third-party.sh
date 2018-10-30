63cb304c2e45fa7965ce234ab712a1c99f6dfd9f
Gregory Abram <gda@login1.hikari.tacc.utexas.edu>
2018-10-30 14:28:18 -0500

    both lib and lib64 when installing third-party libs

--- install-third-party.sh
+++ install-third-party.sh
@@ -4,12 +4,14 @@ echo INSTALL-THIRD-PARTY $*
 tpdir=$1/third-party
 shift
 
-echo XXXXXXXXXXXXXXX $* XCCCCCCCCCCCCCCCC
-
 if test ! -e $tpdir ; then
   mkdir $tpdir
 fi
 
 for i in $* ; do
-  cp -R $i/lib/* $tpdir
+  for j in lib lib64 ; do
+    if test -e $i/$j ; then
+      cp -R $i/$j/*  $tpdir
+    fi
+  done
 done
