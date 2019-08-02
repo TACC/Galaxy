73c81a26677e349108ae3999f17325ee055774ef
David Honegger Rogers <dhr@lanl.gov>
2019-08-02 08:23:57 -0600

    added tools directory

---
+++ tools/plot_vectors.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import sys
+
+LARGE =  100000
+SMALL = -100000 
+
+inputfile = sys.argv[1] 
+data = []
+xmin = LARGE
+ymin = LARGE
+zmin = LARGE
+xmax = SMALL
+ymax = SMALL
+zmax = SMALL
+
+def record_minmax(v):
+    global xmin, xmax, ymin, ymax, zmin, zmax
+
+    if (xmin > v[0]):
+        xmin = v[0]
+    if (xmin > v[3] + v[0]):
+        xmin = v[3] + v[0]
+    if (xmax < v[0]):
+        xmax = v[0]
+    if (xmax < v[3] + v[0]):
+        xmax = v[3] + v[0]
+    if (ymin > v[1]):
+        ymin = v[1]
+    if (ymin > v[4] + v[1]):
+        ymin = v[4] + v[1]
+    if (ymax < v[1]):
+        ymax = v[1]
+    if (ymax < v[4] + v[1]):
+        ymax = v[4] + v[1]
+    if (zmin > v[2]):
+        zmin = v[2]
+    if (zmin > v[5] + v[2]):
+        zmin = v[5] + v[2]
+    if (zmax < v[2]):
+        zmax = v[2]
+    if (zmax < v[5] + v[2]):
+        zmax = v[5] + v[2]
+
+with open(inputfile, "r") as vectors:
+    for line in vectors:
+        vector = [float(i) for i in line.strip().split(",")]
+        data.append(vector)
+        record_minmax(vector)
+
+# print("x {},{}".format(xmin, xmax))
+# print("y {},{}".format(ymin, ymax))
+# print("z {},{}".format(zmin, zmax))
+
+soa = np.array(data)
+
+x, y, z, u, v, w = zip(*soa)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(x, y, z, u, v, w, arrow_length_ratio = 0.2 )
+ax.scatter(x, y, z, color="black")
+ax.set_xlim([xmin, xmax])
+ax.set_ylim([ymin, ymax])
+ax.set_zlim([zmin, zmax])
+plt.show()
---
+++ tools/readme.md
@@ -0,0 +1,7 @@
+# Tools for galaxy debugging, etc.
+
+- ```plot_vectors.py``` Given a csv file of vectors, plot them. The file is a properly formatted ```csv``` file, with the following values (the vector is plotted from ```origin``` to ```origin + delta```)
+
+```
+x origin, y origin, z origin, x delta, y delta, z delta
+```
