09b75c667746aa363d37e914de88b0617a0f07b3
Paul Navratil <pnav@tacc.utexas.edu>
2019-08-21 15:34:38 -0500

    reenabled osx travis compile and disabled osx image testing

--- tests/image-gold-tests.sh
+++ tests/image-gold-tests.sh
@@ -149,13 +149,13 @@ PDIFF_OPTIONS="-fov 85"
 TESTS=0
 FAILS=0
 
-MPI_COMMAND=""
-report "Running single process tests..."
-${GXY_CREATE_PARTITION_DOC} -v radial-0-eightBalls.vol 1 > partition.json
-${GXY_PARTITION_VTUS} partition.json streamlines.vtu eightBalls-points.vtu oneBall-mesh.vtu
-run_tests
-
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+  MPI_COMMAND=""
+  report "Running single process tests..."
+  ${GXY_CREATE_PARTITION_DOC} -v radial-0-eightBalls.vol 1 > partition.json
+  ${GXY_PARTITION_VTUS} partition.json streamlines.vtu eightBalls-points.vtu oneBall-mesh.vtu
+  run_tests
+
   MPI_COMMAND="mpirun -np 2"
   report "Running MPI tests with command '${MPI_COMMAND}'..."
   ${GXY_CREATE_PARTITION_DOC} -v radial-0-eightBalls.vol 2 > partition.json
