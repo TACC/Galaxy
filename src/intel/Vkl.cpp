9cb34bb1a52e30f37655d978c2bbd7b1ea968d67
Greg Abram <gda@tacc.utexas.edu>
2021-01-06 10:57:16 -0700

    renamed embree source dir to intel b/c it'll have vkl as well as embree

---
+++ src/intel/Vkl.cpp
@@ -0,0 +1,82 @@
+// ========================================================================== //
+// Copyright (c) 2014-2020 The University of Texas at Austin.                 //
+// All rights reserved.                                                       //
+//                                                                            //
+// Licensed under the Apache License, Version 2.0 (the "License");            //
+// you may not use this file except in compliance with the License.           //
+// A copy of the License is included with this software in the file LICENSE.  //
+// If your copy does not contain the License, you may obtain a copy of the    //
+// License at:                                                                //
+//                                                                            //
+//     https://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+// Unless required by applicable law or agreed to in writing, software        //
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  //
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.           //
+// See the License for the specific language governing permissions and        //
+// limitations under the License.                                             //
+//                                                                            //
+// ========================================================================== //
+
+#include <embree3/rtcore.h>
+#include <iostream>
+
+#include "Vkl.h"
+
+namespace gxy
+{
+
+KEYED_OBJECT_CLASS_TYPE(Vkl)
+
+static Vkl *theVkl;
+Vkl *GetVkl() { return theVkl; }
+
+void
+Vkl::Register()
+{
+    RegisterClass();
+}
+
+Vkl::~Vkl()
+{
+    vklShutdown();
+}
+
+void
+Vkl::initialize()
+{
+    super::initialize();
+
+    device = vklNewDriver("ispc");
+    vklDriverSetErrorCallback(device, vklError, (void *)this);
+
+    theVkl = this;
+}
+
+int
+Vkl::serialSize()
+{
+    return super::serialSize();
+}
+
+unsigned char *
+Vkl::serialize(unsigned char *p)
+{
+    p = super::serialize(p);
+    return p;
+}
+
+unsigned char *
+Vkl::deserialize(unsigned char *p)
+{
+    p = super::deserialize(p);
+    return p;
+}
+
+bool 
+Vkl::local_commit(MPI_Comm c)
+{
+    return false;
+}
+
+}
---
+++ src/intel/VklVolume.cpp
@@ -0,0 +1,54 @@
+// ========================================================================== //
+// Copyright (c) 2014-2020 The University of Texas at Austin.                 //
+// All rights reserved.                                                       //
+//                                                                            //
+// Licensed under the Apache License, Version 2.0 (the "License");            //
+// you may not use this file except in compliance with the License.           //
+// A copy of the License is included with this software in the file LICENSE.  //
+// If your copy does not contain the License, you may obtain a copy of the    //
+// License at:                                                                //
+//                                                                            //
+//     https://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+// Unless required by applicable law or agreed to in writing, software        //
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  //
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.           //
+// See the License for the specific language governing permissions and        //
+// limitations under the License.                                             //
+//                                                                            //
+// ========================================================================== //
+
+#include "VklVolume.h"
+
+using namespace gxy;
+
+VklVolume::VklVolume(VolumeDPtr v)
+{
+  vec3i counts;
+  v->get_ghosted_local_counts(counts.x, counts.y, counts.z);
+
+  vec3f origin, spacing;
+  v->get_ghosted_local_origin(origin.x, origin.y, origin.z);
+  v->get_deltas(spacing.x, spacing.y, spacing.z);
+
+  size_t sz = counts.x*counts.y*counts.z;
+  VKLData data = vklNewData(sz, v->isFloat() ? VKL_FLOAT : VKL_UCHAR, (void *)v->get_samples().get(), VKL_DATA_SHARED_BUFFER, 0);
+
+  VKLData attr[] = {data};
+  VKLData aData = vklNewData(1, VKL_DATA, attr, VKL_DATA_DEFAULT, 0);
+  vklRelease(data);
+  
+  volume = vklNewVolume("structuredRegular");
+  vklSetVec3i(volume, "dimensions", counts.x, counts.y, counts.z);
+  vklSetVec3f(volume, "gridOrigin", origin.x, origin.y, origin.z);
+  vklSetVec3f(volume, "gridSpacing", spacing.x, spacing.y, spacing.z);
+  vklSetData(volume, "data", aData);
+  vklRelease(aData);
+}
+
+VklVolume::~VklVolume()
+{
+  vklRelease(volume);
+  volume = NULL;
+}
+
