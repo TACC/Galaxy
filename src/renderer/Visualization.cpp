f63a61feed1b36769f24befe41029ceac59436c8
Greg Abram <gda@tacc.utexas.edu>
2024-07-10 11:53:07 -0500

    CHECKBOX macro

--- src/renderer/Visualization.cpp
+++ src/renderer/Visualization.cpp
@@ -131,67 +131,6 @@ Visualization::LoadVisualizationsFromJSON(Value& v)
   return visualizations;
 }
 
-#define CHECKBOX(XX)                                                            \
-  for (vector<VisP>::iterator it = XX.begin(); it != XX.end(); it++)            \
-  {                                                                              \
-    VisP v = *it;                                                                \
-                                                                                 \
-    KeyedDataObjectP kdop = v->GetTheData();                                    \
-    if (! kdop) std::cerr << "WARNING: NULL KeyedDataObjectP" << endl;          \
-                                                                                \
-    Box *l = kdop->get_local_box();                                              \
-    Box *g = kdop->get_global_box();                                            \
-                                                                                \
-    if (first)                                                                  \
-    {                                                                            \
-      first = false;                                                            \
-                                                                                \
-      local_box = *l;                                                            \
-      global_box = *g;                                                          \
-                                                                                \
-      for (int i = 0; i < 6; i++)                                                \
-        neighbors[i] = kdop->get_neighbor(i);                                    \
-    }                                                                            \
-    else if (1 == 0)                                                                       \
-    {                                                                            \
-      // These checks are disabled because the partition boundaries are calculated      \
-      // in slightly different ways causing infinitesimal, but non-zero differences     \
-                                                                                    \
-      if (local_box != *l || global_box != *g)                                  \
-      {                                                                          \
-        APP_PRINT(<< "ERROR: XXDatasets partitioning mismatch between participants:\n"    \
-            << "\n" << "local xyz_min: " << local_box.xyz_min.x << " " << local_box.xyz_min.y << " " << local_box.xyz_min.z \
-            << "\n" << "local xyz_max: " << local_box.xyz_max.x << " " << local_box.xyz_max.y << " " << local_box.xyz_max.z \
-            << "\n" << "*l xyz_min: " << l->xyz_min.x << " " << l->xyz_min.y << " " << l->xyz_min.z \
-            << "\n" << "*l xyz_max: " << l->xyz_max.x << " " << l->xyz_max.y << " " << l->xyz_max.z \
-            << "\n" << "global xyz_min: " << global_box.xyz_min.x << " " << global_box.xyz_min.y << " " << global_box.xyz_min.z \
-            << "\n" << "global xyz_max: " << global_box.xyz_max.x << " " << global_box.xyz_max.y << " " << global_box.xyz_max.z \
-            << "\n" << "*g xyz_min: " << g->xyz_min.x << " " << g->xyz_min.y << " " << g->xyz_min.z \
-            << "\n" << "*g xyz_max: " << g->xyz_max.x << " " << g->xyz_max.y << " " << g->xyz_max.z); \
-        exit(1);                                                                \
-      }                                                                          \
-      for (int i = 0; i < 6; i++)                                                \
-      {                                                                          \
-        if (neighbors[i] != kdop->get_neighbor(i))                              \
-         {                                                                      \
-          APP_PRINT(<< "ERROR: Datasets partitioning mismatch between participants:\n"   \
-                    << "N: " << neighbors[0] << " " \
-                             << neighbors[1] << " " \
-                             << neighbors[2] << " " \
-                             << neighbors[3] << " " \
-                             << neighbors[4] << " " \
-                             << neighbors[5] << "\n"  \
-                    << "K: " << kdop->get_neighbor(0) << " "  \
-                             << kdop->get_neighbor(1) << " "  \
-                             << kdop->get_neighbor(2) << " "  \
-                             << kdop->get_neighbor(3) << " "  \
-                             << kdop->get_neighbor(4) << " "  \
-                             << kdop->get_neighbor(5) << "\n"); \
-          exit(1);                                                              \
-        }                                                                        \
-      }                                                                          \
-    }                                                                            \
-  }
 
 bool 
 Visualization::local_commit(MPI_Comm c)
@@ -201,18 +140,66 @@ Visualization::local_commit(MPI_Comm c)
   for (auto v : vis)
     v->local_commit(c);
 
+  for (vector<VisP>::iterator it = vis.begin(); it != vis.end(); it++)
+  {
+    VisP v = *it;
 
-  CHECKBOX(vis)
+    KeyedDataObjectP kdop = v->GetTheData();
+    if (! kdop) std::cerr << "WARNING: NULL KeyedDataObjectP" << endl;
 
-#if 0
-  std::cerr << "V " << std::hex << ((long)this) << std::dec << 
-      " " << neighbors[0] << 
-      " " << neighbors[1] << 
-      " " << neighbors[2] << 
-      " " << neighbors[3] << 
-      " " << neighbors[4] << 
-      " " << neighbors[5] << "\n";
-#endif
+    Box *l = kdop->get_local_box();
+    Box *g = kdop->get_global_box();
+
+    if (first)
+    {
+      first = false;
+
+      local_box = *l;
+      global_box = *g;
+
+      for (int i = 0; i < 6; i++)
+        neighbors[i] = kdop->get_neighbor(i);
+    }
+    else if (1 == 0)
+    {
+      // These checks are disabled because the partition boundaries are calculated
+      // in slightly different ways causing infinitesimal, but non-zero differences
+
+      if (local_box != *l || global_box != *g)
+      {
+        APP_PRINT(<< "ERROR: XDatasets partitioning mismatch between participants:\n"
+            << "\n" << "local xyz_min: " << local_box.xyz_min.x << " " << local_box.xyz_min.y << " " << local_box.xyz_min.z
+            << "\n" << "local xyz_max: " << local_box.xyz_max.x << " " << local_box.xyz_max.y << " " << local_box.xyz_max.z
+            << "\n" << "*l xyz_min: " << l->xyz_min.x << " " << l->xyz_min.y << " " << l->xyz_min.z
+            << "\n" << "*l xyz_max: " << l->xyz_max.x << " " << l->xyz_max.y << " " << l->xyz_max.z
+            << "\n" << "global xyz_min: " << global_box.xyz_min.x << " " << global_box.xyz_min.y << " " << global_box.xyz_min.z
+            << "\n" << "global xyz_max: " << global_box.xyz_max.x << " " << global_box.xyz_max.y << " " << global_box.xyz_max.z
+            << "\n" << "*g xyz_min: " << g->xyz_min.x << " " << g->xyz_min.y << " " << g->xyz_min.z
+            << "\n" << "*g xyz_max: " << g->xyz_max.x << " " << g->xyz_max.y << " " << g->xyz_max.z);
+        exit(1);
+      }
+      for (int i = 0; i < 6; i++)
+      {
+        if (neighbors[i] != kdop->get_neighbor(i))
+         {
+          APP_PRINT(<< "ERROR: Datasets partitioning mismatch between participants:\n"
+                    << "N: " << neighbors[0] << " "
+                             << neighbors[1] << " "
+                             << neighbors[2] << " "
+                             << neighbors[3] << " "
+                             << neighbors[4] << " "
+                             << neighbors[5] << "\n"
+                    << "K: " << kdop->get_neighbor(0) << " "
+                             << kdop->get_neighbor(1) << " "
+                             << kdop->get_neighbor(2) << " "
+                             << kdop->get_neighbor(3) << " "
+                             << kdop->get_neighbor(4) << " "
+                             << kdop->get_neighbor(5) << "\n");
+          exit(1);
+        }
+      }
+    }
+  }
 
   return false;
 }
